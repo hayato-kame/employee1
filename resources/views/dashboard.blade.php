5c84bb7add96a6aed6c267dba9e321c6455e5b8f
hayato-kame <66853293+hayato-kame@users.noreply.github.com>
2021-03-19 12:50:52 +0900

    welcome dashboard blade ok

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,18 +4,52 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
-    {{-- もともとあったのをコメントアウト --}}
-
-    {{-- <div class="py-12">
+    
+    {{-- ログインしていたら、リンクを表示する --}}
+    {{-- @auth
+        <div class="text-center">                      
+            <ul style="margin-top: 30px;  list-style: none;">
+            <li><a href="{{ url('/users') }}" class="text-lg text-gray-700 underline">ユーザー一覧画面</a></li>
+            <li><a href="{{ url('/departments') }}" class="text-lg text-gray-700 underline">部署一覧画面</a></li>
+            <li><a href="{{ url('/') }}" class="text-lg text-gray-700 underline">従業員一覧画面</a></li>
+            </ul>        
+        </div>        
+    @endauth --}}
+    
+    <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                
+                <div class="grid grid-cols-1 md:grid-cols-2">
+                    <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
+                        <div class="ml-12">
+                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                {{-- Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. 
+                                Check them out, see for yourself, and massively level up your development skills in the process. --}}
+                                <li><a href="{{ url('/users') }}" class="text-lg text-gray-700 underline">ユーザー一覧画面</a></li>
+                            
+                        </div>
+                    </div>
+                </div>
+                    
+                    <div class="p-6 border-t border-gray-200 dark:border-gray-700">
+                        <div class="ml-12">
+                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                            {{-- Laravel News is a community driven portal and newsletter aggregating all
+                             of the latest and most important news in the Laravel ecosystem, including new package releases and tutorials. --}}
+                        
+                             <li><a href="{{ url('/departments') }}" class="text-lg text-gray-700 underline">部署一覧画面</a></li>
+                             <li><a href="{{ url('/') }}" class="text-lg text-gray-700 underline">従業員一覧画面</a></li>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+            {{-- もともとあったのをコメントアウト --}}
+            {{-- <x-jet-welcome /> --}}
             </div>
         </div>
-    </div> --}}
-
-    {{-- リンクを追加 --}}
-    
+    </div>
+   
 
 </x-app-layout>
